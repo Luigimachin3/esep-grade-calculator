bd16151597fed1a8f2b3303efecdf4422facaadf
Luigimachin3 <martinez.lu17@gmail.com>
2025-10-01 09:20:47 -0400

    Part 4 - Add unit tests to reach 100% code coverage

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,12 +34,45 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
-func TestGetGradeF(t *testing.T) {
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 79, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 75, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 69, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 65, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+
+func TestGetGradeA_MultipleScores(t *testing.T) {
 	expected_value := "A"
 
 	gradeCalculator := NewGradeCalculator()
 
-	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
+	gradeCalculator.AddGrade("open source assignment", 99, Assignment)
 	gradeCalculator.AddGrade("exam 1", 95, Exam)
 	gradeCalculator.AddGrade("essay on ai ethics", 91, Essay)
 
@@ -49,3 +82,39 @@ func TestGetGradeF(t *testing.T) {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
+
+func TestGetGradeF(t *testing.T) {
+	expected_value := "F"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 59, Assignment)
+	gradeCalculator.AddGrade("exam 1", 45, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 30, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetEmptyGrade(t *testing.T) {
+	gradeCalculator := NewGradeCalculator()
+
+	actual_value := gradeCalculator.GetFinalGrade()
+	
+	if actual_value != "F" {
+		t.Errorf("Expected F , got '%s' instead", actual_value)
+	}
+}
+
+func TestGetString(t *testing.T) {
+
+	if Assignment.String() == "" {
+		t.Errorf("Must enter a non-empty string")
+	}
+
+}
+
+
